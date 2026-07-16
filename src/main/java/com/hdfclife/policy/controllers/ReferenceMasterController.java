81e39806129f554754604cdb3ff9ccfda710efd1
Nash <nashrdsouza@gmail.com>
2026-07-17 01:49:33 +0530

    added: reference master options

--- src/main/java/com/hdfclife/policy/controllers/ReferenceMasterController.java
+++ src/main/java/com/hdfclife/policy/controllers/ReferenceMasterController.java
@@ -1,11 +1,27 @@
 package com.hdfclife.policy.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hdfclife.policy.service.ReferenceMasterService;
 
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/reference-master")
 public class ReferenceMasterController {
+    private final ReferenceMasterService referenceMasterService;
+
+    @GetMapping("/{category}")
+    public List<?> getReferenceData(@PathVariable String category) {
+        return referenceMasterService.getReferenceMasterList(category);
+    }
+    
 
 }
---
+++ src/main/java/com/hdfclife/policy/service/ReferenceMasterService.java
@@ -0,0 +1,24 @@
+package com.hdfclife.policy.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class ReferenceMasterService {
+
+    private final List<Integer> policyTerms  =  List.of(10, 15, 20, 25, 30);
+    private final List<String> paymentFrequencies = List.of("Monthly", "Quarterly", "Half-Yearly", "Yearly");
+
+    public List<?> getReferenceMasterList(String category){
+        if(category.equalsIgnoreCase("policyterm")){
+            return policyTerms;
+        }else if(category.equalsIgnoreCase("paymentfrequency")){
+            return paymentFrequencies;
+        }else{
+            throw new IllegalArgumentException("Invalid category: " + category);
+        }
+    }
+    
+}
